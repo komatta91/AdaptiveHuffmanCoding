a98facc6323a6892671a53915afce76089ffadb5
komatta91 <komatta91@gmail.com>
2016-03-19 22:23:45 +0100

    testy

---
+++ bin/runTest.bat
@@ -0,0 +1,6 @@
+AdaptiveHuffmanCoding.exe t001.txt 
+AdaptiveHuffmanCoding.exe t001.txt.kf -r
+AdaptiveHuffmanCoding.exe t002.txt
+AdaptiveHuffmanCoding.exe t002.txt.kf -r
+AdaptiveHuffmanCoding.exe t003.bmp 
+AdaptiveHuffmanCoding.exe t003.bmp.kf -r
